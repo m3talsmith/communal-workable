2374692b31361ae8a0f97b86eb434380bc669308
LordbyronIII <brian@rebelhold.com>
2012-06-19 11:56:09 -0400

    added the delete a epic

--- app/controllers/projects/epics_controller.rb
+++ app/controllers/projects/epics_controller.rb
@@ -1,6 +1,7 @@
 class Projects::EpicsController < ProjectsController
   before_filter :force_user
   before_filter :find_project
+  before_filter :find_epic, except: [:new, :create, :index]
 
   def new
     @epic = @project.epics.new
@@ -10,4 +11,15 @@ class Projects::EpicsController < ProjectsController
     @epic = @project.epics.create params[:epic]
     redirect_to [@project, @epic]
   end
+
+  def destroy
+    @epic.destroy
+    redirect_to [@project]
+  end
+
+  private
+    def find_epic
+      epic_id = params[:epic_id].present? ? params[:project_id] : params[:id]
+      @epic = Epic.find(epic_id)
+    end
 end
