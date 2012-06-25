7401950b5d75255ac2d79754cb51bc707873afe3
LordbyronIII <brian@rebelhold.com>
2012-06-25 12:11:18 -0400

    added the ability to transfer funds between accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,15 +7,21 @@ class Account
   belongs_to :project
   has_many :transactions
 
-  def add_funds amount
-    transactions.create amount: amount
-  end
-
   def balance
     transactions.map(&:amount).sum
   end
 
+  def add_funds amount
+    transactions.create amount: amount
+  end
+
   def withdraw amount
     add_funds -amount
+  end
+
+  def transfer details
+    to_account = Account.find(details[:account])
+    to_account.add_funds details[:amount]
+    self.withdraw details[:amount] 
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,6 +4,7 @@ describe Account do
   context 'with an account' do
     before do
       @account = FactoryGirl.create :account, user: @user
+      @account2 = FactoryGirl.create :account, user: @user 
     end
 
     it 'adds funds' do
@@ -22,6 +23,22 @@ describe Account do
       @account.reload
       @account.transactions.count.should == 2
       @account.balance.should == 0.0
+    end
+
+
+    it 'transfers funds' do
+      @account.add_funds 1.0
+      @account.balance.should == 1.0
+      @account.transfer(amount: 1.0, account: @account2.id)  
+
+      @account.reload
+      @account2.reload
+
+      @account.transactions.count.should == 2
+      @account2.transactions.count.should == 1
+      
+      @account.balance.should == 0.0
+      @account2.balance.should == 1.0
     end
   end
 end
